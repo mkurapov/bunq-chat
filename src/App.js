d820d865cb26e6571e5dc8ffed2b07abc1f90b58
Max Kurapov <maxkurapov@gmail.com>
2019-11-15 17:26:23 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,102 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import Login from './Login';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            value: '',
+            user: {},
+            users: [],
+            isLoggedIn: false,
+            messages: [],
+        };
+    }
+
+    componentDidMount() {
+      fetch("http://assignment.bunq.com/users")
+        .then(res => res.json())
+        .then(res => this.setState({users: res}));
+      
+        const currentUsers = localStorage.getItem('users');
+        console.log(currentUsers);
+
+        if (!currentUsers) {
+          
+        } else {
+          
+        }       
+
+
+
+        const {endpoint} = this.state;
+    }
+
+    componentWillUnmount() {
+      
+    }
+
+    handleChange(event) {
+      this.setState({value: event.target.value});
+    }
+  
+    handleSubmit(event) {
+      this.setState(state => {
+        const messages = [...state.messages, state.value];
+        return {
+          messages,
+          value: '',
+        };
+      });
+      event.preventDefault();
+    }
+
+    renderMessages() {
+      return this.state.messages.map((msg, i) => (
+        <li key={i}>{msg}</li>
+      ));
+    }
+
+    onSelectUser(user) {
+      this.setState({
+        user: user,
+        isLoggedIn: true
+      });
+      console.log(user);
+    }
+
+    renderUsers() {
+      return this.state.users.map((user, i) => (
+        <div key={i} onClick={() => this.onSelectUser(user)}>{user.name}</div>
+      ));
+    }
+
+    render() {
+
+      if (this.state.isLoggedIn) {
+        return (
+          <div>
+
+          <form onSubmit={ev => this.handleSubmit(ev)}>
+            <label>
+              Essay:
+              <input type="text" value={this.state.value} onChange={ev => this.handleChange(ev)} />
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
+          
+            { this.renderMessages() }
+          </div>
+        )
+      }
+      else {
+        return (
+          <div>
+          { this.renderUsers() }
+          </div>
+        );
+      }
+    }
 }
 
 export default App;
