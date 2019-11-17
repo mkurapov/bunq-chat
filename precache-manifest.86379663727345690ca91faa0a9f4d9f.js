427c2a52a63ed07e215c296779a15d46bc8e728a
Max Kurapov <maxkurapov@gmail.com>
2019-11-17 18:20:04 +0100

    Updates

---
+++ precache-manifest.86379663727345690ca91faa0a9f4d9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40ebcad23e2653678677bfb935df8669",
+    "url": "/bunq-chat/index.html"
+  },
+  {
+    "revision": "23b112642aacabf8274b",
+    "url": "/bunq-chat/static/css/main.8737e09f.chunk.css"
+  },
+  {
+    "revision": "4f65adfa4e4a180453e5",
+    "url": "/bunq-chat/static/js/2.6399fef8.chunk.js"
+  },
+  {
+    "revision": "23b112642aacabf8274b",
+    "url": "/bunq-chat/static/js/main.6f1ce4eb.chunk.js"
+  },
+  {
+    "revision": "700a59e5029a74004ef6",
+    "url": "/bunq-chat/static/js/runtime-main.99243b25.js"
+  },
+  {
+    "revision": "eb9aa403c01b8517c50a5ff3f329c2a3",
+    "url": "/bunq-chat/static/media/logo.eb9aa403.png"
+  }
+]);
