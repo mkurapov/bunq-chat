e3b920cb3b831c219b8b325486eadad7cf52a46b
Max Kurapov <maxkurapov@gmail.com>
2019-11-17 18:56:35 +0100

    removing https

--- src/const.js
+++ src/const.js
@@ -1,3 +1,3 @@
-export const API_ENDPOINT = 'https://assignment.bunq.com';
+export const API_ENDPOINT = 'http://assignment.bunq.com';
 export const CONVERSATION_ID = 2232;
 export const BUNQ_COLORS = ['#6f42c1', '#007bff', '#28a745', '#dc3545', '#FFC107'];
